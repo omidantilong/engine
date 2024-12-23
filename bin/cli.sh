b87dadaf4f41a93d5bb78a69e1fe1c14f486fbcf
Omid Kashan <omid@antilong.com>
2024-12-23 18:06:34 +0000

    feat: add commands to cli

--- bin/cli.sh
+++ bin/cli.sh
@@ -1,1 +1,16 @@
-echo $@
+#/bin/bash
+
+case $1 in 
+  "build")
+    docker buildx build --tag tenant-image .
+  ;;
+  "serve")
+    docker run --name tenant -i -t -p 8020:8020 tenant-image
+  ;;
+  "copy-externals")
+    mkdir -p dist/node_modules/
+    mkdir -p dist/engine/
+    cp -r node_modules/{react,react-dom,scheduler} dist/node_modules/
+    cp -r engine/{paths,refs}.json dist/engine
+  ;;
+esac
